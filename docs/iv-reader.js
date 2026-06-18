b69dd75f7077b8725912ba551dbdea72de91e8f0
Luan Caldeira <luanmcaldeira@gmail.com>
2026-06-18 12:52:30 -0300

    feat: glue de canvas + harness de integracao + gabarito

--- docs/iv-reader.js
+++ docs/iv-reader.js
@@ -154,7 +154,13 @@
     return { ok: true, atk: a, def: d, sta: s };
   }
 
-  const api = { rgbToHsv, classifyPixel, fillRatioToIV, detectBars, lerIVdeImageData };
+  function lerIVdoCanvas(canvas) {
+    const ctx = canvas.getContext('2d');
+    const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return lerIVdeImageData(img);
+  }
+
+  const api = { rgbToHsv, classifyPixel, fillRatioToIV, detectBars, lerIVdeImageData, lerIVdoCanvas };
 
   if (typeof module !== 'undefined' && module.exports) module.exports = api;
   else global.IVReader = api;
